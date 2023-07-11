29d06eda6c5090907ca1551bcfde6b22b780e075
Muhammad Raihandaffa <128868157+MuhammadDzikrianasa@users.noreply.github.com>
2023-07-12 00:28:38 +0700

    Update and rename login.html to home.php

---
+++ home.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+    <div class="header">
+          <h2>Portal SMA NEGERI 1</h2>
+        </div>
+
+        <div class="topnav">
+          <a >Home</a>
+          <a href="presensi.php">Presensi Siswa</a>
+          <a href="#">Akun</a>
+        </div>
+
+        <div class="row">
+          <div class="leftcolumn">
+            <div class="card">
+              <h2>SMA Negeri 1</h2>
+              <div class="fakeimg" style="height:475px;"><h3>Visi dan Misi SMA Negeri 1</h3>
+              <h5>Visi SMA Negeri 1</h5>
+              <p>“Berprestasi dilandasi Iman, Taqwa dan Berbudaya Lingkungan serta Berwawasan Global”</p>
+              <h5>Misi SMA Negeri 1</h5>
+              <p>1. Mewujudkan pendidikan untuk menghasilkan prestasi dan lulusa berkwalitas tinggi yang peduli dengan lingkungan hidup.
+              <br>2. Mewujudkan sumber daya manusia yang beriman, produktif, kreatif, inofatif dan efektif.
+              <br>3. Mewujudkan pengembangan inovasi pembelajaran sesuai tuntutan.
+              <br>4. Mewujudkan sumber daya manusia yang peduli dalam mencegahan pencemaran, mencegahan kerusakan lingkungan dan melestarikan lingkungan hidup.
+              <br>5. Mewujudkan sarana prasarana reprensentatif dan up to date.
+              <br>6. Mewujudkan pengelolaan pendidikan yang professional.
+              <br>7. Mewujudkan sistim penilaian yang berafiliasi</p>
+              </div>
+            </div>
+          </div>
+          <div class="rightcolumn">
+            <div class="card">
+              <h3>Info Akademik</h3>
+              <div class="fakeimg"><p>MASA KEGIATAN BELAJAR MENGAJAR<br>
+                <br>11 JULI 2022 - 23 JUNI 2023</p></div>
+            </div>
+          </div>
+        </div>
+        <div class="footer">
+          <p>Copyright 2023</p>
+        </div>
+
+    </body>
+</html>
